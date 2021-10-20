94b0e7c2176862cb41a5fa81edd5c655d2bec9a6
paulusnguyenanh <paulusnguyenanh@gmail.com>
2021-10-20 14:44:31 +0700

    my Love

---
+++ config.js
@@ -0,0 +1,9 @@
+const nameGirl = 'Thanh đáng yêu';
+const giftUrl = 'https://m.me/nguyenanh711';
+const eventName = 'Chúc Mừng 20-10';
+const titleCard = 'Gửi đến Thanh';
+const contentCard = 'Chúc bé Thanh 20/10 tràn ngập niềm vui và những nụ cười. Hãy luôn là chính mình Thanh nhé <3 <3 <3 ';
+
+const giftImage = 'Thanh.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
